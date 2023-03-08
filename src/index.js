09dfe4fe1c90f41a7b265b981251cc59e97d094e
Liz Burton <liz.burton147@gmail.com>
2023-03-08 10:54:49 -0500

    creates todo list functionality

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.getElementById('create-task-form')
+  form.addEventListener('submit',(e) => {
+    e.preventDefault()
+    createToDo(form.new_task.value)
+    form.reset()
+  })
 });
+
+function createToDo(toDo){
+  let li = document.createElement('li')
+  li.textContent = toDo + " "
+  let btn = document.createElement('button')
+  btn.textContent = "X"
+  btn.addEventListener("click",handleDelete)
+  li.appendChild(btn)
+  document.getElementById('tasks').append(li)
+}
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
