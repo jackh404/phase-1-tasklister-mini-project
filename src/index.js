7a691d59eb865dabd25b0cb868dd75f121916f2d
Jack Henza <jackhenza@gmail.com>
2023-10-04 15:34:29 -0400

    created edit functionality

--- src/index.js
+++ src/index.js
@@ -1,24 +1,38 @@
+debugger
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
   const form = document.getElementById('create-task-form')
   form.addEventListener('submit',(e) => {
     e.preventDefault()
-    createToDo(form.new_task.value)
+    createToDo(form.new_task.value,form.duration.value)
     form.reset()
   })
 });
 //creates list item, calls prioritization function, and adds list item to
 //the todo list
-function createToDo(toDo){
+function createToDo(toDo,duration){
   //get list html objects
   const toDoBlock = document.getElementById('tasks')
   const list = toDoBlock.getElementsByTagName('li')
   //create new list item
   let li = document.createElement('li')
-  li.textContent = toDo + " "
+  li.description = toDo
+  li.duration = duration
+  li.textContent = toDo + ": "
+  //add duration estimate
+  let dur = document.createElement('span')
+  dur.textContent = duration + " minutes "
+  li.appendChild(dur)
+  //create edit button
+  let edit = document.createElement('button')
+  edit.textContent = "✎"
+  edit.classList.add('delete')
+  edit.addEventListener("click",handleEdit)
+  li.appendChild(edit)
   //create delete button
   let btn = document.createElement('button')
   btn.textContent = "X"
+  btn.classList.add('delete')
   btn.addEventListener("click",handleDelete)
   li.appendChild(btn)
   //check priority level of other items in the list to place new item
@@ -50,4 +64,14 @@ function prioritize(li){
       li.classList.add('whenever')
       break
   }
+}
+function handleEdit(e){
+  const form = document.getElementById('create-task-form')
+  const description = e.target.parentNode.description
+  const duration = e.target.parentNode.duration
+  const priority = e.target.parentNode.priority
+  form.new_task.value = description
+  form.duration.value = duration
+  form.priority.value = priority
+  handleDelete(e);
 }
